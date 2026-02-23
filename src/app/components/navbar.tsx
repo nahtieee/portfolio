81644757cd120da9c368b0bd0eb6478899a45066
EthanDith <ethandith@gmail.com>
2026-02-22 19:50:30 -0800

    fix

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { useState, useEffect, useRef } from "react";
 import Link from "next/link";
+import Image from "next/image";
 
 export default function Navbar() {
   const [open, setOpen] = useState(false);
